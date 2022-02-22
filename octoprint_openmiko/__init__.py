d09eedaef4a64463a01149578dba14f20e233769
Tom Mount <tmountjr@gmail.com>
2022-02-22 11:39:56 -0500

    initial commit after local dev

---
+++ octoprint_openmiko/__init__.py
@@ -0,0 +1,57 @@
+# coding=utf-8
+from __future__ import absolute_import
+
+import octoprint.plugin
+
+
+class OpenmikoPlugin(octoprint.plugin.AssetPlugin):
+
+    # ~~ AssetPlugin mixin
+
+    def get_assets(self):
+        return {
+            "js": ["js/openmiko.js"],
+            "css": ["css/openmiko.css"],
+        }
+
+    # ~~ Softwareupdate hook
+
+    def get_update_information(self):
+        # Define the configuration for your plugin to use with the Software Update
+        # Plugin here. See https://docs.octoprint.org/en/master/bundledplugins/softwareupdate.html
+        # for details.
+        return {
+            "openmiko": {
+                "displayName": "OpenMiko Plugin",
+                "displayVersion": self._plugin_version,
+
+                # version check: github repository
+                "type": "github_release",
+                "user": "tmountjr",
+                "repo": "OctoPrint-Openmiko",
+                "current": self._plugin_version,
+
+                # update method: pip
+                "pip": "https://github.com/tmountjr/OctoPrint-Openmiko/archive/{target_version}.zip",
+            }
+        }
+
+
+# If you want your plugin to be registered within OctoPrint under a different name than what you defined in setup.py
+# ("OctoPrint-PluginSkeleton"), you may define that here. Same goes for the other metadata derived from setup.py that
+# can be overwritten via __plugin_xyz__ control properties. See the documentation for that.
+__plugin_name__ = "OpenMiko Plugin"
+
+
+# Set the Python version your plugin is compatible with below. Recommended is Python 3 only for all new plugins.
+# OctoPrint 1.4.0 - 1.7.x run under both Python 3 and the end-of-life Python 2.
+# OctoPrint 1.8.0 onwards only supports Python 3.
+__plugin_pythoncompat__ = ">=3,<4"  # Only Python 3
+
+__plugin_implementation__ = OpenmikoPlugin()
+
+def __plugin_load__():
+    global __plugin_hooks__
+    __plugin_hooks__ = {
+        "octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
+    }
---
+++ octoprint_openmiko/static/js/openmiko.js
@@ -0,0 +1,100 @@
+/*
+ * View model for OctoPrint-OpenMiko
+ *
+ * Author: Tom Mount
+ * License: MIT
+ */
+$(function() {
+    function OpenmikoViewModel(parameters) {
+        var self = this;
+        self.settingsViewModel = parameters[1];
+
+        self.getAdditionalControls = function() {
+            var streamUrl = self.settingsViewModel.settings.webcam.streamUrl();
+            var parsedUrl = new URL(streamUrl);
+            var apiUrl = parsedUrl.protocol + '//' + parsedUrl.hostname+ ':8081/api';
+
+            /**
+             * Post a value to a url.
+             * @param {*} url The URL to post to.
+             * @param {*} data The value to post.
+             * @returns {Promise}
+             */
+            function post(url, data) {
+                return new Promise(function(resolve, reject) {
+                    var xhr = new XMLHttpRequest();
+                    xhr.open('POST', url);
+                    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+                    xhr.onload = function() {
+                        if (xhr.status >= 200 && xhr.status < 300) {
+                            resolve(xhr.response);
+                        } else {
+                            reject({
+                                status: xhr.status,
+                                statusText: xhr.statusText,
+                                url: xhr.url,
+                            });
+                        }
+                    };
+                    xhr.onerror = function() {
+                        reject({
+                            status: xhr.status,
+                            statusText: xhr.statusText,
+                            url
+                        });
+                    };
+                    xhr.send('value=' + data);
+                });
+            }
+
+            /**
+             * Enable Day Mode by switching off the IR leds and enabling the IR cutoff filter.
+             */
+            var dayMode = function() {
+                var urls = [
+                    { url: apiUrl + '/ir_cut', value: 1 },
+                    { url: apiUrl + '/ir_led', value: 0 },
+                ];
+                Promise.allSettled(urls.map(u => post(u.url, u.value)))
+                    .catch(function(e) {
+                        console.log('Failure.');
+                        console.log(e);
+                    })
+                    .then(function() {
+                        console.log('Success!');
+                    });
+            };
+
+            /**
+             * Enable Night Mode by switching on the IR leds and disabling the IR cutoff filter.
+             */
+            var nightMode = function() {
+                var urls = [
+                    { url: apiUrl + '/ir_cut', value: 0},
+                    { url: apiUrl + '/ir_led', value: 1},
+                ];
+                Promise.all(urls.map(u => post(u.url, u.value)))
+                    .catch(function(e) {
+                        console.log('Failure.');
+                        console.log(e);
+                    })
+                    .then(function() {
+                        console.log('Success!');
+                    });
+            };
+
+            return [
+                { name: "OpenMiko", type: "section", layout: "horizontal", children: [
+                    { name: "Day Mode", type: "javascript", javascript: dayMode, enabled: true },
+                    { name: "Night Mode", type: "javascript", javascript: nightMode, enabled: true }
+                ], },
+            ];
+        };
+    }
+
+    OCTOPRINT_VIEWMODELS.push({
+        construct: OpenmikoViewModel,
+        dependencies: ["settingsViewModel"],
+        elements: [],
+    });
+});
